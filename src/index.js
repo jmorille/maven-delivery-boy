c805a9d27a9690f81d9817888c38aa84a7e67089
jmorille <jmorille@gmail.com>
2018-06-06 23:55:05 +0200

    ignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 'use strict';
+const fs = require('fs');
 const downloadArtifacts  = require('maven-id-resolver');
 
 console.log(downloadArtifacts);
-//downloadArtifacts(['org.apache.commons:commons-lang3:3.4']);
+downloadArtifacts(['org.apache.commons:commons-lang3:3.4']).then(res => {
+    console.log(res);
+})
