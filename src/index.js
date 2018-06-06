74743336b6ec462f0510f8a85d06fa57ceb2172a
jmorille <jmorille@gmail.com>
2018-06-07 00:02:46 +0200

    watch

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ console.log(downloadArtifacts);
 downloadArtifacts(['org.apache.commons:commons-lang3:3.4']).then(res => {
     console.log(res);
 })
+
+fs.watch('./in', (eventType, filename) => {
+    console.log(eventType, "==> ", filename);
+// could be either 'rename' or 'change'. new file event and delete
+// also generally emit 'rename'
+//    console.log(filename);
+})
